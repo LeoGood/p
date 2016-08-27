a06607bb29b498a44e44671d099127e4006488c8
LeoGood <leogood@foxmail.com>
2016-08-27 20:36:31 +0800

    config/routes.rb change /

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/homg'
-
-  get 'pages/home'
-
-  get 'pages/home'
     get "home" =>"pages#home"
   get "about" =>"pages#about"
 
