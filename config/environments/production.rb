1dffcea6b6505427f2c996a16998550ce23937e3
LeoGood <leogood@foxmail.com>
2016-09-02 23:10:10 +0800

    Add account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#required for Heroku
+#note to actual host
+  config.action_mailer.default_url_options = { :host => 'http://pbyleogood.herokuapp.com/' }
+
+
+
 end
